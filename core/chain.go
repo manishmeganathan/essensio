304fa04fc166ff273df0e286bd07afe3ab3cab56
Manish Meganathan <meganathan.manish@gmail.com>
2022-07-21 12:36:48 +0530

    Refactor BlockChain
    - Refactor Blockchain code to use new Block generator methods

--- core/chain.go
+++ core/chain.go
@@ -1,20 +1,29 @@
 package core
 
+import "github.com/manishmeganathan/essensio/common"
+
+// BlockChain represents a blockchain as a set of Blocks
 type BlockChain struct {
 	Blocks []*Block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
+// AddBlock generates and appends a Block to the chain for a given data.
+// Returns the Block hash after appending it.
+func (chain *BlockChain) AddBlock(data string) common.Hash {
 	// Get the hash of the previous block
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	prevHash := prevBlock.BlockHash
 
 	// Create a new Block and append it
-	block := NewBlock(data, prevHash)
+	block := NewBlock(data, prevHash, prevBlock.BlockHeight+1)
 	chain.Blocks = append(chain.Blocks, block)
+
+	return block.BlockHash
 }
 
+// NewBlockChain returns a new BlockChain with an initialized
+// Genesis Block with the provided genesis data.
 func NewBlockChain(genesis string) *BlockChain {
-	genesisblock := NewBlock(genesis, []byte{})
+	genesisblock := NewBlock(genesis, []byte{}, 0)
 	return &BlockChain{[]*Block{genesisblock}}
 }
