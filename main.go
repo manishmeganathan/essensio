69abf5ad8b655106e35de1784b95da74b799dcfc
Manish Meganathan <meganathan.manish@gmail.com>
2022-07-24 17:55:42 +0530

    Add simple CLI to main.go
    - Added a simple os.args based CLI
    - Added a addblock and showchain command

--- main.go
+++ main.go
@@ -2,20 +2,63 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/manishmeganathan/essensio/core"
 )
 
 func main() {
-	chain := core.NewBlockChain("Genesis Information")
+	// Check if a command has been entered
+	if len(os.Args) < 2 {
+		log.Fatalln("Command Not Found")
+	}
 
-	chain.AddBlock("Data 1")
-	chain.AddBlock("Data 2")
-	chain.AddBlock("Data 3")
+	// Read the command and check its value
+	switch cmd := os.Args[1]; cmd {
+	// AddBlock command
+	case "addblock":
+		// Check if an input has been provided for the block data
+		if len(os.Args) < 3 {
+			log.Fatalln("Missing Input for 'AddBlock' command")
+		}
 
-	fmt.Println("====")
-	for _, block := range chain.Blocks {
-		fmt.Println(block)
-		fmt.Println("====")
+		// Load up the BlockChain
+		chain, err := core.NewBlockChain()
+		if err != nil {
+			log.Fatalln("Failed to Start Blockchain:", err)
+		}
+
+		// Add the Block with the given data to the chain
+		if err := chain.AddBlock(os.Args[2]); err != nil {
+			log.Fatalln("Failed to Add Block to Chain:", err)
+		}
+
+	// ShowChain command
+	case "showchain":
+		// Load up the BlockChain
+		chain, err := core.NewBlockChain()
+		if err != nil {
+			log.Fatalln("Failed to Start Blockchain:", err)
+		}
+
+		// Create a new iterator for the chain
+		iterator := chain.NewIterator()
+		fmt.Print("\nFull Chain Information\n\n")
+
+		// Iterate until the chain is done
+		for !iterator.Done() {
+			// Get the next block
+			block, err := iterator.Next()
+			if err != nil {
+				log.Fatalln("Iterator Error:", err)
+			}
+
+			// Print the Block
+			fmt.Println(block)
+		}
+
+	default:
+		log.Fatalf("Unsupported Command: %v\n", cmd)
 	}
 }
